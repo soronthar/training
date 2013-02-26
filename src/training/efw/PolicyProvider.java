50e4f8a4cc9a7d258c9887c805c791e765c0fdc7
soronthar <soronthar>
2013-02-26 15:12:02 -0430

    Commit de extracccion

--- src/training/efw/PolicyProvider.java
+++ src/training/efw/PolicyProvider.java
@@ -1,12 +1,17 @@
 package training.efw;
 
 import com.consisint.acsele.openapi.policy.Policy;
+import com.consisint.acsele.openapi.policy.RiskUnit;
+import com.consisint.acsele.openapi.policy.RiskUnitList;
 import com.consisint.acsele.reports.providers.ReportProvider;
 
 import java.util.Arrays;
 import java.util.Vector;
 
 public class PolicyProvider extends ReportProvider {
+    public static final int RU_DATA_LEVEL = 4;
+    public static final int RU_ID_LEVEL = 2;
+    public static final int POLICY_ID_LEVEL = 0;
     Policy policy;
 
     public Vector loadPolicyIds(String ids[]) {
@@ -14,21 +19,54 @@ public class PolicyProvider extends ReportProvider {
     }
 
     public String getPolicyNumber(String ids[]) {
-        String policyNumber=ids[0];
-        this.policy=Policy.loadByPolicyNumber(policyNumber);
+        String policyNumber=ids[POLICY_ID_LEVEL];
+        ensurePolicyLoaded(policyNumber);
         return policyNumber;
+    }
+
+    private void ensurePolicyLoaded(String policyNumber) {
+        if (this.policy==null || !policy.getPolicyNumber().equals(policyNumber)) {
+            this.policy= Policy.loadByPolicyNumber(policyNumber);
+        }
     }
 
     public Vector loadPolicyProperties(String ids[]) {
         return new Vector(policy.getDynamicData().getMetaData().getData().keySet());
     }
 
-    public String getPolicyPropertyName(String ids[]) {
-        return ids[2] ;
+    public String getPolicyPropertyName(String ids[],String levelParam) {
+        int level=Integer.valueOf(levelParam);
+        return ids[level] ;
     }
 
     public String getPolicyPropertyValue(String ids[]) {
-        return policy.getDynamicData().getInput(ids[2]);
+        return policy.getDynamicData().getInput(ids[RU_ID_LEVEL]);
+    }
+
+    public Vector loadRUProperties(String ids[]) {
+        RiskUnit riskUnit = this.policy.getRiskUnit(ids[RU_ID_LEVEL]);
+        return new Vector(riskUnit.getDynamicData().getMetaData().getData().keySet());
+    }
+
+    public String getRUPropertyName(String ids[]) {
+        return ids[Integer.valueOf(RU_DATA_LEVEL)] ;
+    }
+
+    public String getRUPropertyValue(String ids[]) {
+        RiskUnit riskUnit = this.policy.getRiskUnit(ids[RU_ID_LEVEL]);
+        return riskUnit.getDynamicData().getInput(ids[RU_DATA_LEVEL]);
+    }
+
+
+    public Vector<String> loadRUIds(String ids[]) {
+        ensurePolicyLoaded(ids[POLICY_ID_LEVEL]);
+        Vector<String> result=new Vector<String>();
+
+        RiskUnitList riskUnits = policy.listRU();
+        for (RiskUnit riskUnit : riskUnits) {
+            result.add(Long.toString(riskUnit.getId()));
+        }
+        return result;
     }
 
     @Override
